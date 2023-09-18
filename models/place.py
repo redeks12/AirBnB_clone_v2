bac06c619636608a68d9c072a8b5a0c82e4b83d8
ikede-divine-keno <ikededivine@gmail.com>
2023-09-18 08:11:47 -0700

    places.py

--- models/place.py
+++ models/place.py
@@ -10,7 +10,7 @@ class Place(BaseModel):
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
-    description = ""
+    description = Column(String(1024), nullable=True)
     number_rooms = 0
     number_bathrooms = 0
     max_guest = 0
