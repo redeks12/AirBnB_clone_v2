8236a34d64bba1e0d8a9b6d1cc4628f2bc211126
ikede-divine-keno <ikededivine@gmail.com>
2023-09-18 07:39:33 -0700

    Task 7 user.py

--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+import models
+import os
+from models.base_model import BaseModel, Base
 
 
 class User(BaseModel, Base):
