dd1fd70ca48751764c57373f0d8d698aa5e37f24
kumdan job <redeks123456@gmail.com>
2023-09-20 17:23:38 +0100

    task 10: push for testing

--- models/amenity.py
+++ models/amenity.py
@@ -18,4 +18,6 @@ class Amenity(BaseModel, Base):
         place_amenities = relationship(
             "Place",
             secondary=place_amenity,
+            back_populates="amenities",
+            viewonly=False,
         )
