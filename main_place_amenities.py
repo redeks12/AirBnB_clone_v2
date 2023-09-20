34009f1587f7b1983201f0b13ec5a8537c3a8d41
kumdan job <redeks123456@gmail.com>
2023-09-20 16:32:54 +0100

    task 10: push for testing

--- main_place_amenities.py
+++ main_place_amenities.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 """ Test link Many-To-Many Place <> Amenity
 """
-from models.base_model import BaseModel
-from models.__init__ import storage
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+from models import *
 
 
 # creation of a State
