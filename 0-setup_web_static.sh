034e17bf470037338d3be5a58d600093193eeac9
kumdan job <redeks123456@gmail.com>
2023-10-04 14:56:21 +0100

    m

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,23 +7,23 @@ if ! command -v nginx &> /dev/null; then
 fi
 
 if [ ! -d /data/ ]; then
-    mkdir /data/
+    sudo mkdir /data/
 fi
 
 if [ ! -d /data/web_static/ ]; then
-    mkdir /data/web_static/
+    sudo mkdir /data/web_static/
 fi
 
 if [ ! -d /data/web_static/releases/ ]; then
-    mkdir /data/web_static/releases/
+    sudo mkdir /data/web_static/releases/
 fi
 
 if [ ! -d /data/web_static/shared/ ]; then
-    mkdir /data/web_static/shared/
+    sudo mkdir /data/web_static/shared/
 fi
 
 if [ ! -d /data/web_static/releases/test ]; then
-    mkdir /data/web_static/releases/test
+    sudo mkdir /data/web_static/releases/test
 fi
 $myhtml = "<!DOCTYPE html>
 <html lang='en'>
@@ -34,12 +34,12 @@ $myhtml = "<!DOCTYPE html>
 </body>
 </html>"
 
-touch /data/web_static/releases/test/index.html
-echo $myhtml > /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html
+sudo echo $myhtml > /data/web_static/releases/test/index.html
 
 if [ -d /data/web_static/current/ ]; then
-    rm /data/web_static/current/
+    sudo rm /data/web_static/current/
 fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data
