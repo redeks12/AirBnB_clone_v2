c0f8dfba4d18cc0435b137cb1f9c6f6fe16d07ba
kumdan job <redeks123456@gmail.com>
2023-10-05 00:10:48 +0100

    m

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Write a Fabric script that generates a .tgz archive from the contents of
 the web_static folder of your AirBnB Clone repo, using the function do_pack."""
-from fabric import task, Connection
+from fabric import Connection
+from fabric.tasks import task
 
 
 @task
