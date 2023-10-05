b87ed1ab56c27256ee92f3c26e93a3bc8e3d00ae
kumdan job <redeks123456@gmail.com>
2023-10-05 01:29:21 +0100

    m

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers,
+using the function do_deploy:"""
+
+import os
+
+from fabric.api import env, put, run, task, parallel, sudo
+
+
+@task
+@parallel
+def do_deploy(archive_path):
+    """Prototype: def do_deploy(archive_path):
+    Returns False if the file at the path archive_path doesn’t exist
+    The script should take the following steps:
+    Upload the archive to the /tmp/ directory of the web server
+    Uncompress the archive to the folder /data/web_static/releases/<archive filename without extension> on the web server
+    Delete the archive from the web server"""
+    if not os.path.exists(archive_path):
+        return False
+    env.hosts = ["54.145.85.177", "100.25.17.121"]
+    put(archive_path, "/tmp/")
+    fl = archive_path.split("/")[1].split(".")[0]
+    run("tar -xzvf /data/web_static/releases/{}".format(fl))
+    run("rm -r /tmp/*.tgz")
+    run("rm -r /data/web_static/current")
+    sudo("ln -s /data/web_static/releases/{} /data/web_static/current".format(fl))
+    return True
