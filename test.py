d68f2b0a284326ceb60a1cd1e0ab4ad5d810f6f6
kumdan job <redeks123456@gmail.com>
2023-10-20 05:52:34 -0700

    script

--- test.py
+++ test.py
@@ -32,6 +32,7 @@ city_2_1.save()
 """
 print("")
 all_states = storage.all(State)
-for state_id, state in all_states.items():
-    for city in state.cities:
-        print("Find the city {} in the state {}".format(city, state))
+print(all_states.items())
+#for state_id, state in all_states.items():
+#    for city in state.cities:
+#        print("Find the city {} in the state {}".format(city, state))
