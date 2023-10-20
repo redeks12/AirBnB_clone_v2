fa4d27005740bc9f63d4ac253da58e61b41b477e
kumdan job <redeks123456@gmail.com>
2023-10-20 14:09:56 +0100

    m

--- models/state.py
+++ models/state.py
@@ -4,23 +4,26 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
+import os
 
 
 class State(BaseModel, Base):
     """State class"""
 
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete")
+    if os.environ.get("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
 
-    @property
-    def cities(self):
-        """Return the cities"""
-        from models import storage
+        @property
+        def cities(self):
+            """Return the cities"""
+            from models import storage
 
-        cit = []
-        store = storage.all(City)
-        for key, city in store:
-            if self.id == city.state_id:
-                cit.append(city)
-        return cit
+            cit = []
+            store = storage.all(City)
+            for key, city in store:
+                if self.id == city.state_id:
+                    cit.append(city)
+            return cit
